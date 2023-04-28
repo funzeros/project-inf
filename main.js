a028541ce0705d5865fd4f015d1c2ef00efaf492
方珂琛 <984512789@qq.com>
2023-04-28 16:40:58 +0800

    fix: vite使用worker修正

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import r from "./lib/request"
 
 
-await r.request('/config/init', { key: 'theme', value: 'light' })
+  ; (async () => {
+    await r.request('/config/init', { key: 'theme', value: 'light' })
 
-r.request('/config/get', { key: 'theme' })
-  .then(async (d) => {
-    console.log(d)
-  })
-  .catch((e) => {
-    console.error(e)
-  })
+    r.request('/config/get', { key: 'theme' })
+      .then(async (d) => {
+        console.log(d)
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+  })()
